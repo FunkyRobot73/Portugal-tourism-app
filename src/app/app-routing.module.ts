df435d4ab278f3c9f5918bc02046f6ebd5cfcafa
Carlos <arlo252@gmail.com>
2023-04-04 20:10:12 -0400

    add routing to array

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{path: "weather/:name/: springN/:springM/: summerN/: summerM/: winterN/:winterM/: autumnN/: autumnM/: visitFrom/: visitTo", component: WeatherDetailsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
